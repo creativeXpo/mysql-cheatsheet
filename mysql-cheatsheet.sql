549ce4af14fd4dfb8d4f932ceb29a3e4ae86f014
Golam Kibria <soysob87@gmail.com>
2025-11-07 23:45:07 +0600

    Update mysql-cheatsheet.sql

--- mysql-cheatsheet.sql
+++ mysql-cheatsheet.sql
@@ -1,2 +1,30 @@
--- To create a Database
-CREATE DATABASE zoo
+-- To create a Database: 
+CREATE DATABASE institute; 
+
+-- List of all the databases that THE user has permission to access: 
+SHOW DATABASES;
+
+-- To use a specified database:
+USE institute;
+
+-- To Delete a Specified database: 
+DROP DATABASE institute;
+
+-- To list all tables in the database: 
+SHOW TABLES;
+
+-- To get information about a specified table: 
+DESCRIBE student;
+DESC student;
+SHOW COLUMNS FROM student;
+
+-- To create a table: 
+CREATE TABLE student (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  phone VARCHAR(100),
+  course VARCHAR(100),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
